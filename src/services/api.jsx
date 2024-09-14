b6287797c8adedabec88a6dfad2856838d0863ad
Tariq ali <76946087+Tariq-the-Coder@users.noreply.github.com>
2024-09-14 20:38:40 +0530

    Update api.jsx

--- src/services/api.jsx
+++ src/services/api.jsx
@@ -1,7 +1,7 @@
 // api.js
 import axios from 'axios';
 
-const API_URL = 'http://localhost:5000/api';
+const API_URL = `https://ksolves-hackathon-backend.onrender.com/api`;
 
 const api = axios.create({
   baseURL: API_URL,
